fed3dcc84a900cdd22f963262ceb439dd9322624
Agnissan Isaac Valen <valenbouge@gmail.com>
2026-03-12 12:54:22 +0000

    jj

--- js/data.js
+++ js/data.js
@@ -55,29 +55,3 @@ const articles = {
 
 
 
-var swiper = new Swiper(".news-cont", {
-      spaceBetween: 20,
-      loop: true,
-      centeredSlides: true,
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
-        breakpoints: {
-            0: {
-                slidesPerView: 1,
-            },
-            568: {
-                slidesPerView: 2,
-            },
-            768: {
-                slidesPerView: 2,
-            },
-            1020: {
-                slidesPerView: 3,
-            },
-            
-           
-        },
-            
-    });
